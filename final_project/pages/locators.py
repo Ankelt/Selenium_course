5f87dabe061ea2e41a532d2259ea54f0d516e08a
me <123>
2021-11-18 19:56:17 +0300

    add tests for product page about login

--- final_project/pages/locators.py
+++ final_project/pages/locators.py
@@ -6,10 +6,6 @@ class BasePageLocators():
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
